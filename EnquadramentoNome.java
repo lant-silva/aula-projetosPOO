46dc62b9306c716cacffbabde743394c6e95bf64
Luiz <125385378+lant-silva@users.noreply.github.com>
2023-08-17 16:57:53 -0300

    Create EnquadramentoNome.java

---
+++ EnquadramentoNome.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Inicio {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("digite seu nome");
+		String nome = input.nextLine();
+		
+		String linha = "";
+		int separacao = 5;
+		
+		for(int i=0;i<separacao;i++) {
+			nome = " "+nome;
+		}
+		for(int i=0;i<separacao;i++) {
+			nome = nome+" ";
+		}
+		nome = "#"+nome+"#";
+		int tamanhoNome = nome.length();
+		
+		for(int i=0;i<tamanhoNome;i++) {
+			linha += "#";
+		}
+		
+		System.out.println("la vem a magica :D");
+		
+		System.out.println(linha);
+		System.out.println(nome);
+		System.out.println(linha);
+		input.close();
+	}
+}
